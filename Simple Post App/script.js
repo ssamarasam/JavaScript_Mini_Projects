2e7dbc8a62d6ffc38e96056aefeec8818760e26a
ssamarasam <samcsegct@gmail.com>
2023-05-22 13:13:50 +0530

    mini javascript apps

---
+++ Simple Post App/script.js
@@ -0,0 +1,52 @@
+console.log("script is working");
+
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("button clicked");
+
+  formValidation();
+});
+
+let data = {};
+function acceptData() {
+  data["text"] = input.value;
+  console.log(data);
+  createPost();
+}
+let formValidation = () => {
+  if (input.value === "") {
+    msg.innerHTML = "Post cannot be empty";
+    console.log("input not entered");
+  } else {
+    msg.innerHTML = "";
+    console.log("success");
+    acceptData();
+  }
+};
+
+let createPost = () => {
+  posts.innerHTML += `
+    <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onClick="editPost(this)" class="fas fa-edit"></i>
+      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+  </div>`;
+
+  input.innerHTML = "";
+};
+
+let deletePost = (e) => {
+  e.parentElement.parentElement.remove();
+};
+
+let editPost = (e) => {
+  input.value = e.parentElement.previousElementSibling.innerHTML;
+  e.parentElement.parentElement.remove();
+};
